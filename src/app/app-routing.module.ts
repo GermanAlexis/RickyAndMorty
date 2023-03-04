918713b96303597e10fea7f628ab12fc62daac27
German Alexis <alexisdager93@hotmail.com>
2023-03-04 13:27:32 -0500

    Fix style - implement of header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,30 +2,31 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'characters', pathMatch: 'full' },
   {
     path: 'home',
     loadChildren: () =>
       import('./pages/home/home.module').then((m) => m.HomeModule),
   },
   {
-    path: 'notfound',
-    loadChildren: () =>
-      import('./pages/not-found/not-found.module').then(
-        (m) => m.NotFoundModule
-      ),
-  },
-  {
-    path: 'character-details',
+    path: 'character-details/:id',
     loadChildren: () =>
       import(
         './pages/characters/characters-details/characters-details.module'
       ).then((m) => m.CharactersDetailsModule),
   },
   {
-    path: 'character-list',
+    path: 'characters',
     loadChildren: () =>
       import('./pages/characters/characters-list/characters-list.module').then(
         (m) => m.CharactersListModule
+      ),
+  },
+  {
+    path: '**',
+    loadChildren: () =>
+      import('./pages/not-found/not-found.module').then(
+        (m) => m.NotFoundModule
       ),
   },
 ];
