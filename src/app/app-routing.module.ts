8d8f39823fea33fc4762a7e0ed451f00bb6023f8
German Alexis <alexisdager93@hotmail.com>
2023-03-04 11:37:48 -0500

    Add modules needs by app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./pages/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'notfound',
+    loadChildren: () =>
+      import('./pages/not-found/not-found.module').then(
+        (m) => m.NotFoundModule
+      ),
+  },
+  {
+    path: 'character-details',
+    loadChildren: () =>
+      import(
+        './pages/characters/characters-details/characters-details.module'
+      ).then((m) => m.CharactersDetailsModule),
+  },
+  {
+    path: 'character-list',
+    loadChildren: () =>
+      import('./pages/characters/characters-list/characters-list.module').then(
+        (m) => m.CharactersListModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
