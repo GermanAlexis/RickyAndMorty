d0ca821692c1220d43605fcd67a096618d4276a8
German Alexis <alexisdager93@hotmail.com>
2023-03-06 22:39:38 -0500

    Add trasnlate configure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
 import {
+  HttpClient,
   HttpClientModule,
   HTTP_INTERCEPTORS,
-  withInterceptors,
 } from '@angular/common/http';
 import { HeaderModule } from './shared/components/header/header.module';
 import { SpinnerInterceptor } from './shared/interceptor/spinner-interceptor';
 import { SpinnerModule } from './shared/components/spinner/spinner.module';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [AppComponent],
@@ -23,6 +28,13 @@ import { SpinnerModule } from './shared/components/spinner/spinner.module';
     HttpClientModule,
     HeaderModule,
     SpinnerModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
   ],
   providers: [
     {
--- src/app/pages/characters/characters-card/characters-card.module.ts
+++ src/app/pages/characters/characters-card/characters-card.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CharactersCardComponent } from './characters-card.component';
 import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [CharactersCardComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, TranslateModule],
   exports: [CharactersCardComponent],
 })
 export class CharactersCardModule {}
